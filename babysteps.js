c76f57ecabe6e8977bb0462378ff70c847a77585
Briceida <BriceidaMariscal@gmail.com>
2017-07-31 11:02:13 -0700

    changed let to const when suitable

--- babysteps.js
+++ babysteps.js
@@ -1,5 +1,5 @@
-let newNumArr = process.argv;
-let arrLength = process.argv.length;
+const newNumArr = process.argv;
+const arrLength = process.argv.length;
 let total = 0;
 for (let i = 2; i < arrLength; i++){
     total += Number(newNumArr[i])
