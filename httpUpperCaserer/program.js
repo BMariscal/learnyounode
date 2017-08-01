9aad6f04d69ced3995256218b9f3ab3aada7a099
Briceida <BriceidaMariscal@gmail.com>
2017-08-01 11:15:20 -0700

    http server turns POST request body to upper case

--- httpUpperCaserer/program.js
+++ httpUpperCaserer/program.js
@@ -1,3 +1,13 @@
+/*
+  Write an HTTP server that receives only POST requests and converts
+  incoming POST body characters to upper-case and returns it to the client.
+
+  Your server should listen on the port provided by the first argument to
+  your program.
+*/
+
+
+
 const http = require('http');
 const map = require('through2-map');
 const port = Number(process.argv[2]);
