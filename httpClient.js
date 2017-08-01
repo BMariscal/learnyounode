fbeca3bb8d7da8985512bdfba66d433c24b1bc87
Briceida <BriceidaMariscal@gmail.com>
2017-07-31 18:46:02 -0700

    http client

--- httpClient.js
+++ httpClient.js
@@ -2,11 +2,11 @@
   Write a program that performs an HTTP GET request to a URL provided to you
   as the first command-line argument. Write the String contents of each
   "data" event from the response to a new line on the console (stdout).
+
 */
 
-
 const http = require('http');
-const url = process.argv[2];
+const url =  process.argv[2];
 http.get(url, (response)=>{
         response.setEncoding('utf8');
         response.on("error", (err)=> console.error(err))
